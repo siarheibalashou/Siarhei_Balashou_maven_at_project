697831cad63c13f4a6fd0d044563f8de99b65f29
Siarhei Balashou <siarhei.balashou@yandex.by>
2024-04-02 21:29:10 +0300

    123

---
+++ src/main/java/day12/TestedClass.java
@@ -0,0 +1,17 @@
+package day12;
+
+public class TestedClass {
+    public int getNumber(int x, int y) {
+        int z = 0;
+        if (x < 0 && y < 10) {
+            z = x - y + 100;
+        } else {
+            z = x - y + 100;
+        }
+
+        if (y < 0 || z > 50) {
+            z = (z - y) / 2;
+        }
+        return x + y + z;
+    }
+}
---
+++ src/test/java/StatementCoverage.java
@@ -0,0 +1,16 @@
+import day12.TestedClass;
+
+public class StatementCoverage {
+    public static void main(String[] args) {
+        TestedClass testedClass = new TestedClass();
+//if (x < 0 && y < 10) -> (y < 0 || z > 50)
+        if (testedClass.getNumber(-1, 9) == 48) {
+            System.out.println("passed");
+        } else System.out.println("failed");
+
+// else -> (y < 0 || z > 50)
+        if (testedClass.getNumber(1, 12) == 51) {
+            System.out.println("passed");
+        } else System.out.println("false");
+    }
+}
