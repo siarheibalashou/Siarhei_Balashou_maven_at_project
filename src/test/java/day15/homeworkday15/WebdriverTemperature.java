74fb2df2541e04be82b7489479f704d1788bd359
Siarhei Balashou <siarhei.balashou@yandex.by>
2024-04-15 01:19:41 +0300

    booking

--- src/test/java/day15/homeworkday15/WebdriverTemperature.java
+++ src/test/java/day15/homeworkday15/WebdriverTemperature.java
@@ -6,11 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 
 public class WebdriverTemperature {
     public static void main(String[] args) throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--lang=en-us");
+        options.addArguments("--lang=en-US");
         WebDriver driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
@@ -18,10 +24,10 @@ public class WebdriverTemperature {
         driver.findElement(By.xpath("//textarea[@class='gLFyf']")).sendKeys("погода минск");
         driver.findElement(By.xpath("//textarea[@class='gLFyf']")).submit();
         driver.findElement(By.xpath("//a[text()='Change to English']")).click();
-        String tomorrowName = driver.findElement(By.xpath("//div[@id='wob_dp']//div[@class='wob_df'][1]//div[@class='Z1VzSb']")).getAttribute("aria-label");
-        String tomorrowTemperatureLocator = String.format("//div[@class='DYO8ie']/*[name()='svg']/*[name()='text' and contains(@aria-label,'Celsius %s 12:00')]", tomorrowName);
-        //  WebElement textElem = driver.findElement(By.xpath(tomorrowTemperatureLocator));
-        // System.out.println("Text from textelem - " + textElem.getText());
+        String tomorrowName = LocalDate.now().plusDays(1).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US);
+        String tomorrowTemperatureLocator = String.format("//*[name()='text' and contains(@aria-label,'Celsius %s 12:00')]", tomorrowName);
+        WebElement textElem = driver.findElement(By.xpath(tomorrowTemperatureLocator));
+        System.out.println("Text from textelem - " + textElem.getText());
         System.out.println(driver.findElement(By.xpath(tomorrowTemperatureLocator)).getAttribute("aria-label"));
     }
 }
